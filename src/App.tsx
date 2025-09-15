e2b665b13f0fcaedecaa9399e5b1c8a0d8706604
설영환 <younghwan@mynormal.co>
2025-09-15 18:23:19 +0900

    fix: 경로 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from '../Home'
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
       <Route path="/" element={<Home />} />
       {/* 예: <Route path="/home" element={<Home />} /> 로 바꾸면 URL은 /home */}
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
